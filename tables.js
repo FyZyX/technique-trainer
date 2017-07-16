761c22adbd40463808a018ac5ec6e0a54486d03a
Brandon Brown <kooolbreez1@users.noreply.github.com>
2017-07-16 16:55:28 -0700

    updated function to correctly work for pHands > 17 and < 7

--- tables.js
+++ tables.js
@@ -14,19 +14,20 @@
     [9,["db","db","db","db","db","h","h","h","h","h"]],
     [8,["h","h","h","db","db","h","h","h","h","h"]],
     [7,["h","h","h","h","h","h","h","h","h","h"]]
-]; // takes 17- total value of players hand and compares to total value of dealers hand - 1.
-//var table = hardTable;
-//console.log(hardTable[1]);
+];
 /**
  * A function that when given the value of the players current hand and the value of the dealers current hand returns the move to be made.
  * @param pHand the total value of the players hand (between 17 & 7).
- * @param dHand the total value of the dealers hand.
+ * @param dHand the total value of the dealers hand (Ace = 11).
  * @returns {*} the move to be made (h: hit, NC: stay, db:double).
  */
 function hardHand(pHand, dHand){
+    if(pHand > 17){
+        pHand = 17;
+    }else if(pHand < 7 ){pHand = 7;}
     var move = hardTable[(17 - pHand)];
     var dealer = move[1];
     return dealer[dHand - 2];
 
 }
-console.log(hardHand(15,6)); //(val of player hand,val of healer hand)
+console.log(hardHand(11,6)); //(val of player hand,val of healer hand)
