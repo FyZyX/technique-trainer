35b75301c135a80d2744492bfc35ab9c67c8aa34
Lucas Lofaro <lucasmlofaro@gmail.com>
2017-07-22 11:49:21 -0700

    started work on soft hand tables

--- tables.js
+++ tables.js
@@ -10,10 +10,14 @@ var hardTable = require('./hardTable');
  * @returns {*} the move to be made (h: hit, NC: stay, db:double).
  */
 function hardHand(pHand, dHand){
+    var table = hardTable;
+    if (pHand[0] === 'A' || pHand[1] === 'A') {
+      table = softTable;
+    }
     let pHandTotal = pHand.reduce(function(a, b) { return a + b; }, 0);
     if (pHandTotal > 17) { pHandTotal = 17; }
     else if (pHandTotal < 7) { pHandTotal = 7; }
-    return hardTable[pHandTotal.toString()][dHand.toString()];
+    return table[pHandTotal.toString()][dHand.toString()];
 }
 console.log(hardHand([8, 3], 6)); //(val of player hand,val of healer hand)
 
